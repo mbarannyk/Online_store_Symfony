e990adbbaf02f1649cf78050e93ca7d5b93fc8a8
mbarannyk <Maryano4ka2008@ukr.net>
2022-07-10 19:01:43 +0300

    addAdmin

--- project/src/Controller/ProfileController.php
+++ project/src/Controller/ProfileController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\OrderRepository;
-use App\Repository\ProductRepository;
-use App\Repository\OrderProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,17 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'profile')]
-    public function UserOrders(OrderProductRepository $OrderProductRepository, OrderRepository $OrderRepository, ProductRepository  $ProductRepository): Response
+    public function UserOrders(): Response
     {
-        $orders = $OrderRepository->findAll();
-        $baskets = $OrderProductRepository->findAll();
-        $products = $ProductRepository->findAll();
         return $this->render('profile.html.twig',
             [
                 'controller_name' => 'ProfileController',
-                'orders' => $orders,
-                'baskets' => $baskets,
-                'products' => $products,
                 'user' => $this->getUser(),
             ]
         );
